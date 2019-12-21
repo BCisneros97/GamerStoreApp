20edb02549f74975eddc69a5b186283d2509b389
Benny Cisneros <benycisneros@gmail.com>
2019-12-20 21:24:38 -0500

    Creación de activities

---
+++ app/src/main/java/com/gamerstore/gamerstoreapp/ListarDiseniosActivity.java
@@ -0,0 +1,13 @@
+package com.gamerstore.gamerstoreapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListarDiseniosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_disenios);
+    }
+}
--- app/src/main/java/com/gamerstore/gamerstoreapp/MainActivity.java
+++ app/src/main/java/com/gamerstore/gamerstoreapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.gamerstore.gamerstoreapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void listar(View view) {
+        Intent intent = new Intent(getApplicationContext(), ListarDiseniosActivity.class);
+        startActivity(intent);
+    }
+
+    public void salir(View view) {
+        finish();
     }
 }
